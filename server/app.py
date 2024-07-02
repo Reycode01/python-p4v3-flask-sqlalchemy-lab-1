1613ff00c52031bac360aaba8c111874154f4bba
humphrey <writernic89@gmail.com>
2024-07-02 12:19:49 +0300

    sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -14,14 +13,55 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    with Session(db.engine) as session:
+        earthquake = session.get(Earthquake, id)
+    
+    if earthquake:
+        response_body = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        response_status = 200
+    else:
+        response_body = {
+            "message": f"Earthquake {id} not found."
+        }
+        response_status = 404
+    
+    response = make_response(jsonify(response_body), response_status)
+    return response
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    response_body = {
+        "count": len(earthquakes),
+        "quakes": [
+            {
+                "id": quake.id,
+                "location": quake.location,
+                "magnitude": quake.magnitude,
+                "year": quake.year
+            } for quake in earthquakes
+        ]
+    }
+    
+    response_status = 200
+    response = make_response(jsonify(response_body), response_status)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+
